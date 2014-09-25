ca1d8e79780d9f76919fb5582f2b849b406eb9ee
Olga Brani <olgabrani@grnet.gr>
2014-09-25 15:15:29 +0300

    snf-ui-app and ui-web ember project

---
+++ synnefo_ui/ui_settings.py
@@ -0,0 +1,32 @@
+# Copyright (C) 2010-2014 GRNET S.A.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+"""
+Settings for the snf-ui-app.
+"""
+
+# --------------------------------------------------------------------
+# Process UI settings
+
+from django.conf import settings
+from collections import OrderedDict
+from synnefo.lib import parse_base_url
+
+BASE_URL = getattr(settings, 'UI_BASE_URL',
+                   'https://synnefo.live/ui/')
+
+BASE_HOST, BASE_PATH = parse_base_url(BASE_URL)
+UI_STATIC_MEDIA_URL = getattr(settings, 'UI_STATIC_MEDIA_URL', settings.MEDIA_URL + 'ui/')
---
+++ synnefo_ui/version.py
@@ -0,0 +1,8 @@
+
+__version__ = "0.16next_12022_e68d41d"
+__version_vcs_info__ = {
+    'branch': 'feature-newui',
+    'revid': 'e68d41d',
+    'revno': 12022}
+__version_user_email__ = "olgabrani@grnet.gr"
+__version_user_name__ = "Olga Brani"
