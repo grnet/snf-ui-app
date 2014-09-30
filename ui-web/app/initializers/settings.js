86ada0188d2ac417b5f7a14cb3f336810edae819
Olga Brani <olgabrani@grnet.gr>
2014-09-30 16:06:47 +0300

    Add singleton=true to settings initializer

--- ui-web/app/initializers/settings.js
+++ ui-web/app/initializers/settings.js
@@ -3,7 +3,7 @@ import Ember from "ember";
 var settings = Ember.Object.extend(SETTINGS);
 
 export var initialize = function(container, app) {
-  app.register('settings:main', settings);
+  app.register('settings:main', settings, {singleton:true});
   app.inject('controller', 'settings', 'settings:main');
 };
 
