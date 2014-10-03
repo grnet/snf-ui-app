b9a48aa462c19236313194a73b38f351143b3d8a
Olga Brani <olgabrani@grnet.gr>
2014-10-03 16:41:36 +0300

    Select with projects in containers view

--- ui-web/app/controllers/container.js
+++ ui-web/app/controllers/container.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
+  title: 'object controller title',
+
+  availableProjects: function(){
+    var that = this;
+    return this.store.filter('project',{}, function(p) {
+      return p.get('id') != that.get('selectedProject').get('id');
+    });
+  }.property('selectedProject'),
+
+  selectedProject: function(){
+    return this.get('model').get('project');
+  }.property('model.project'),
+
+  watchProject: function(){
+    console.log('CHANGED', this.get('model'));
+    console.log(this.get('selectedProject'));
+  }.observes('selectedProject'),
+
 });
