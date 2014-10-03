49451dd737de7a4989c9c3ed734dbfd91958dd58
Olga Brani <olgabrani@grnet.gr>
2014-10-03 22:54:12 +0300

    Show project pithos.diskspace quota

--- ui-web/app/models/project.js
+++ ui-web/app/models/project.js
@@ -1,5 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  name: DS.attr('string')
+  name: DS.attr('string'),
+  homepage: DS.attr('string'),
+  diskspace: DS.attr('string'),
 });
--- ui-web/app/serializers/project.js
+++ ui-web/app/serializers/project.js
@@ -3,12 +3,21 @@ import DS from 'ember-data';
 export default DS.RESTSerializer.extend({
   extractArray: function(store, type, payload) {
     var project_list = payload;
+    project_list.forEach(function(project){
+        project.diskspace = project.resources['pithos.diskspace'].member_capacity;
+    });
     payload = { projects: project_list};
     return this._super(store, type, payload);
   },
   extractSingle: function(store, type, payload, id) {
-    console.log(payload, 'payload');
     payload = { project: payload};
     return this._super(store, type, payload, id);
+  },
+  normalizeHash: {
+    project: function(hash){
+      hash.diskspace = hash.resources['pithos.diskspace'].member_capacity;
+      return hash;
+    }
   }
+
 });
