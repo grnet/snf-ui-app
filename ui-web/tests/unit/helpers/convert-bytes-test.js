21f78f48a386beeb32d2c0cf0c3777da3c78f7d9
Olga Brani <olgabrani@grnet.gr>
2014-10-06 16:05:49 +0300

    Convert bytes to human readable size helper

---
+++ ui-web/tests/unit/helpers/convert-bytes-test.js
@@ -0,0 +1,11 @@
+import {
+  convertBytes
+} from 'ui-web/helpers/convert-bytes';
+
+module('ConvertBytesHelper');
+
+// Replace this with your real tests.
+test('it works', function() {
+  var result = convertBytes(42);
+  ok(result);
+});
