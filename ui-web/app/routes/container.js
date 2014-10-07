1a25cf912e27653943fb7864c49d8280e75422a9
Olga Brani <olgabrani@grnet.gr>
2014-10-07 16:54:16 +0300

    Reload model in container's route.
    Fixes olgabrani/synnefo#13

--- ui-web/app/routes/container.js
+++ ui-web/app/routes/container.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  setupController: function(controller, model) {
+    model.reload();
+    this._super(controller, model);
+  }
 });
