dbeaa0b8164b94ecfcd4295c29ced8e6cd84a7b8
Olga Brani <olgabrani@grnet.gr>
2014-10-14 15:01:20 +0300

    Set property to ObjectsController

--- ui-web/app/controllers/object.js
+++ ui-web/app/controllers/object.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
   view_src: function(){
-    return '/ui/view/'+this.get('settings').get('uuid')+'/pithos/'+this.get('model').get('name');
+    var base_url = this.get('settings').get('storage_view_url');
+    var uuid = this.get('settings').get('uuid');
+    console.log(this.get('container_id'));
+    var container_id = this.get('container_id');
+    var name = this.get('model').get('name');
+    return base_url+uuid+'/'+container_id+name;
   }.property('model.name'),
 });
--- ui-web/app/routes/container.js
+++ ui-web/app/routes/container.js
@@ -5,6 +5,7 @@ export default Ember.Route.extend({
     // if path is not set, then default path is current folder '/'
     var current_path = params.current_path ? params.current_path : '/';
     var container_id = params.container_id;
+    this.set('container_id', params.container_id);
     return this.store.find('object', {container_id: container_id, path: current_path});
     
   },
@@ -14,5 +15,9 @@ export default Ember.Route.extend({
   },
 
   controllerName: 'objects',
+  setupController: function(controller, model) {
+    controller.set('model', model);
+    controller.set('container_id', this.get('container_id'));
+  },
 
 });
