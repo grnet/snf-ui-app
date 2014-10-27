46cf81a29a0c6536f330b935a83e1505a04fa627
Olga Brani <olgabrani@grnet.gr>
2014-10-27 17:35:06 +0200

    Download link for object's previous version
    Fixes olgabrani/synnefo#33

---
+++ ui-web/app/controllers/object/version.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.ObjectController.extend({
+  needs: ['object/versions'],
+
+  view_src: Ember.computed.alias("controllers.object/versions.view_src"),
+
+  view_src_version: function(){
+    return this.get('view_src')+'?version='+this.get('model').get('id');
+  }.property(),
+});
