ba2c438037e0ca3c9cb193c7d89d8befa0209505
Olga Brani <olgabrani@grnet.gr>
2014-11-06 13:58:34 +0200

    Container Sidebar with containers & groups

--- ui-web/app/routes/container.js
+++ ui-web/app/routes/container.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  renderTemplate: function(){
+    this.render('container');
+
+    this.render('groups/sidebar', {
+      into: 'container',
+      outlet: 'sidebarGroups',
+      controller: 'groups',
+      model: this.store.find('group')
+    });
+
+   this.render('containers/sidebar', {
+      into: 'container',
+      outlet: 'sidebarContainers',
+      controller: 'containers',
+      model: this.store.find('container')
+    });
+
+
+  }
 });
