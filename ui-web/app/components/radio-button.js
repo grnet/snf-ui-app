456f9f9f15c823794e080866520918c65900933f
Olga Brani <olgabrani@grnet.gr>
2014-12-11 17:26:22 +0200

    Radio-button component

---
+++ ui-web/app/components/radio-button.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tagName: 'input',
+  type: 'radio',
+  attributeBindings: ['type', 'name', "checked:checked", 'value', 'bindValue'],
+
+  checked : function() {
+    return this.get("value") === this.get("bindValue");   
+  }.property('value', 'bindValue'),
+
+  click : function() {
+    this.set("bindValue", this.$().val());
+    this.sendAction();
+  },
+
+});
