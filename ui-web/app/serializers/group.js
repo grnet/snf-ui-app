159881d288013db006ec5de25ebc2889fe07115c
Kostas Papadimitriou <kpap@grnet.gr>
2015-01-09 17:22:46 +0200

    Handle empty groups case in group serializer

--- ui-web/app/serializers/group.js
+++ ui-web/app/serializers/group.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend({
   extractArray: function(store, type, payload) {
-    payload = {groups: payload.groups};
+    payload = {groups: payload.groups || []};
     return this._super(store, type, payload);
   },
 });
