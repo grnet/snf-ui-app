ddcc284ed93511bdcce2fe2aac87fcb53278f92a
Athina Bekakou <athinabek@grnet.gr>
2015-02-04 20:48:40 +0200

    Create function findUser (based on preexisting code)
    Also, handle error and success of the user request. Keep the result in a
    temporary property that the InputEmailsView observes.

--- ui-web/app/controllers/groups.js
+++ ui-web/app/controllers/groups.js
@@ -29,47 +29,69 @@ export default Ember.ArrayController.extend({
     }
   }.observes('newName'),
 
+  userData: undefined,
   actions: {
-    createGroup: function(){
+    findUser: function(email) {
+      var userEmail = 'email='+email;
       var self = this;
-      var name = this.get('newName');
-      var emails = this.get('newEmails');
-      
-      if (!name.trim()) { return; }
-      if (!emails.trim()) { return; }
+        self.store.find('user', userEmail).then(function(user) {
+          var user = Ember.Object.create({
+            uuid: user.id,
+            email: email,
+            status: 'success'
+          });
 
-      emails = emails.split(',');
-      if (emails.length <1 ) { return; }
-
-      var onSuccess = function() {
-        self.set('newName', '');
-        self.set('newEmails', '');
-      };
-      
-      var onFail = function(reason){
-        console.log('reason:', reason);
-        self.send('showActionFail', reason);
-      };
-
-      // get users by email
-      var users = emails.map(function(email) {
-        var userEmail = 'email='+email.trim();
-        return self.store.find('user', userEmail);
-      });
-
-      // wait until all users are retrieved
-      return Ember.RSVP.all(users).then(function(res){
-        // create a group with no users
-        var group = self.store.createRecord('group', {
-          name: name,
-          id: name,   
+          self.set('userData', user);
+        }, function(error) {
+          var user = Ember.Object.create({
+            uuid: undefined,
+            email: email,
+            status: 'error',
+            errorMsg: error.message
+          });
+          self.set('userData', user);
         });
-        // add users to the newly created group
-        group.get('users').then(function(users){
-          users.pushObjects(res);
-          group.save().then(onSuccess, onFail);
-        });
-      }, onFail);
+    },
+    createGroup: function(){
+      // var self = this;
+      // var name = this.get('newName');
+      // var emails = this.get('newEmails');
+      
+      // if (!name.trim()) { return; }
+      // if (!emails.trim()) { return; }
+
+      // emails = emails.split(',');
+      // if (emails.length <1 ) { return; }
+
+      // var onSuccess = function() {
+      //   self.set('newName', '');
+      //   self.set('newEmails', '');
+      // };
+      
+      // var onFail = function(reason){
+      //   console.log('reason:', reason);
+      //   self.send('showActionFail', reason);
+      // };
+
+      // // get users by email
+      // var users = emails.map(function(email) {
+      //   var userEmail = 'email='+email.trim();
+      //   return self.store.find('user', userEmail);
+      // });
+
+      // // wait until all users are retrieved
+      // return Ember.RSVP.all(users).then(function(res){
+      //   // create a group with no users
+      //   var group = self.store.createRecord('group', {
+      //     name: name,
+      //     id: name,   
+      //   });
+      //   // add users to the newly created group
+      //   group.get('users').then(function(users){
+      //     users.pushObjects(res);
+      //     group.save().then(onSuccess, onFail);
+      //   });
+      // }, onFail);
 
     }
   }
