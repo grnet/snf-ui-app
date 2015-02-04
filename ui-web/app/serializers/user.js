932f7921223aacc30028a327df4f822672e3c82a
Athina Bekakou <athinabek@grnet.gr>
2015-02-04 19:27:55 +0200

    Handle empty server response when a user is not found

--- ui-web/app/serializers/user.js
+++ ui-web/app/serializers/user.js
@@ -3,19 +3,25 @@ import Ember from 'ember';
 
 export default DS.RESTSerializer.extend({
   extract: function(store, type, payload) {
-    if (_.isEmpty(payload.uuid_catalog)){
+    if (_.isEmpty(payload.uuid_catalog) && !_.isEmpty(payload.displayname_catalog)) {
       var userEmail = Ember.keys(payload.displayname_catalog)[0];
+      console.log(userEmail)
       return {
         'id': payload.displayname_catalog[userEmail],
         'uuid': payload.displayname_catalog[userEmail],
         'email': userEmail
       }
     }
-    var userId = Ember.keys(payload.uuid_catalog)[0];
-    return {
-      'id': userId,
-      'uuid': userId,
-      'email': payload.uuid_catalog[userId]
+    else if (!_.isEmpty(payload.uuid_catalog) && _.isEmpty(payload.displayname_catalog)) {
+      var userId = Ember.keys(payload.uuid_catalog)[0];
+      return {
+        'id': userId,
+        'uuid': userId,
+        'email': payload.uuid_catalog[userId]
+      }
+    }
+    else {
+      throw new Error('Not found');
     }
   }
 });
