7b8f4a28322f98907f01df078dfd69f727023741
Athina Bekakou <athinabek@grnet.gr>
2015-02-05 13:00:46 +0200

    Add is_js library

--- ui-web/Brocfile.js
+++ ui-web/Brocfile.js
@@ -6,5 +6,6 @@ app.import('bower_components/moment/moment.js');
 app.import('bower_components/foundation/js/foundation/foundation.js');
 app.import('bower_components/foundation/js/foundation/foundation.reveal.js');
 app.import('bower_components/underscore/underscore.js');
+app.import('bower_components/is_js/is.js');
 
 module.exports = app.toTree();
