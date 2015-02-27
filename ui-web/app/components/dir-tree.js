123b8b2c7436c45a3efc2398fc56e3431f8b7926
Olga Brani <olgabrani@grnet.gr>
2015-02-27 16:57:17 +0200

    Add links to dir-tree containers & folders

--- ui-web/app/components/dir-tree.js
+++ ui-web/app/components/dir-tree.js
@@ -14,6 +14,28 @@ export default Ember.Component.extend({
     }
   }.property('root'),
 
+  is_root: function(){
+    return this.get('root') == '/';
+  }.property('root'),
+
+  is_container: function(){
+    return this.get('root').indexOf('/') == -1;
+  }.property('root'),
+
+  is_folder: function(){
+    return !this.get('is_container') && !this.get('is_root');
+  }.property('is_root', 'is_container'),
+
+  container_id: function(){
+    return this.get('root').split('/').shift();
+  }.property('root'),
+
+  current_path: function(){
+    var arr = this.get('root').split('/');
+    arr.shift();
+    return arr.join('/');
+  }.property('root'),
+
   actions: {
     toggle: function(){
       this.toggleProperty('expanded');
