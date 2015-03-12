e0a558c28b891e5aa46a256abba574e156694983
Athina Bekakou <athinabek@grnet.gr>
2015-03-12 17:01:50 +0200

    Decode group name before saving it

--- ui-web/app/adapters/group.js
+++ ui-web/app/adapters/group.js
@@ -2,10 +2,10 @@ import StorageAdapter from 'ui-web/snf/adapters/storage';
 
 export default StorageAdapter.extend({
 
-  ajaxSuccess: function(jqXHR, jsonPayload) {
+	ajaxSuccess: function(jqXHR, jsonPayload) {
 
-    // get all headers as a string
-    var headers = jqXHR.getAllResponseHeaders().split('\n');
+		// get all headers as a string
+		var headers = jqXHR.getAllResponseHeaders().split('\n');
 
 		// removes the colon and the uuids of the members of each group
 		var re = (/X-Account-Group-\S*(?=:)/g);
@@ -15,7 +15,7 @@ export default StorageAdapter.extend({
 			groupHeader = h.match(re)
 			if(groupHeader) {
 				let group = {};
-				group.id = groupHeader[0].replace('X-Account-Group-', '').toLowerCase();
+				group.id = decodeURI(groupHeader[0].replace('X-Account-Group-', '').toLowerCase());
 				group.name = group.id;
 
 				let uuids = jqXHR.getResponseHeader(groupHeader[0]);
@@ -36,36 +36,36 @@ export default StorageAdapter.extend({
 		});
 
 		jsonPayload.groups = groups;
-    return jsonPayload;
-  },
+		return jsonPayload;
+	},
 
-  updateRecord: function(store, type, record) {
-    var self = this;
-    var headers = this.get('headers');
-    var header = 'X-Account-Group-'+record.get('name');
-    headers['Accept'] = 'text/plain';
-    return record.get('users').then(function(users){
-      headers[header] = users.map(function(user){
-        console.log('user.id', user.id);
-        return user.id;
-      }).join(',');
-      return self.ajax(self.buildURL(type.typeKey)+'?update=', 'POST');
-    });
+	updateRecord: function(store, type, record) {
+		var self = this;
+		var headers = this.get('headers');
+		var header = 'X-Account-Group-'+record.get('name');
+		headers['Accept'] = 'text/plain';
+		return record.get('users').then(function(users){
+			headers[header] = users.map(function(user){
+				console.log('user.id', user.id);
+				return user.id;
+			}).join(',');
+			return self.ajax(self.buildURL(type.typeKey)+'?update=', 'POST');
+		});
 
-  },
+	},
 
-  createRecord: function(store, type, record){
-    return this.updateRecord(store, type, record);
-  },
+	createRecord: function(store, type, record){
+		return this.updateRecord(store, type, record);
+	},
 
-  deleteRecord: function(store, type, record) {
-    var headers = this.get('headers');
-    var header = 'X-Account-Group-'+record.get('name');
-    headers['Accept'] = 'text/plain';
-    headers[header] = '~';
-    return this.ajax(this.buildURL(type.typeKey)+'?update=', 'POST');
+	deleteRecord: function(store, type, record) {
+		var headers = this.get('headers');
+		var header = 'X-Account-Group-'+record.get('name');
+		headers['Accept'] = 'text/plain';
+		headers[header] = '~';
+		return this.ajax(this.buildURL(type.typeKey)+'?update=', 'POST');
 
-  }
+	}
 
 });
 
