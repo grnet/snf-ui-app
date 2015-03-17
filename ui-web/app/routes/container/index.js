abba92a7fa41ac74469bfdb565e3e98675c67d42
Olga Brani <olgabrani@grnet.gr>
2015-03-17 11:49:07 +0200

    Add ResetScrollMixin to container route

--- ui-web/app/routes/container/index.js
+++ ui-web/app/routes/container/index.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import ResetScrollMixin from '../../mixins/reset-scroll';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(ResetScrollMixin, {
   redirect: function(){
     //http://stackoverflow.com/questions/18689446/
     this.transitionTo('objects', '');
