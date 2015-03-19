d16fd3d72f9d48e7cc2019a650f217fd21070d1c
Athina Bekakou <athinabek@grnet.gr>
2015-03-19 18:49:14 +0200

    Fix bugs regarding groups
    The fixed bugs were related with the creation and the deletion of a group.

--- ui-web/app/adapters/group.js
+++ ui-web/app/adapters/group.js
@@ -3,39 +3,44 @@ import StorageAdapter from 'ui-web/snf/adapters/storage';
 export default StorageAdapter.extend({
 
 	ajaxSuccess: function(jqXHR, jsonPayload) {
-
 		// get all headers as a string
-		var headers = jqXHR.getAllResponseHeaders().split('\n');
+		var headersStr = jqXHR.getAllResponseHeaders();
+		var headers = headersStr.split('\n');
 
 		// removes the colon and the uuids of the members of each group
 		var re = (/X-Account-Group-\S*(?=:)/g);
+
 		var groupHeader, groupName, groups=[];
 
-		headers.forEach(function(h) {
-			groupHeader = h.match(re)
-			if(groupHeader) {
-				let group = {};
-				group.id = decodeURI(groupHeader[0].replace('X-Account-Group-', '').toLowerCase());
-				group.name = group.id;
+		if(headersStr.indexOf('X-Account-Group-') > -1) {
+			headers.forEach(function(h) {
+				groupHeader = h.match(re);
 
-				let uuids = jqXHR.getResponseHeader(groupHeader[0]);
+				if(groupHeader) {
+					var group = {};
+					group.id = decodeURI(groupHeader[0].replace('X-Account-Group-', '').toLowerCase());
+					group.name = group.id;
 
-				if(uuids === '~'){
-					return ;
+					var uuids = jqXHR.getResponseHeader(groupHeader[0]);
+
+					if(uuids === '~'){
+						return ;
+					}
+
+					if(uuids) {
+						group.users = uuids.split(',');
+					}
+					else {
+						group.users = [];
+					}
+
+					groups.push(group);
 				}
+			});
 
-				if(uuids) {
-					group.users = uuids.split(',');
-				}
-				else {
-					group.users = [];
-				}
+			jsonPayload.groups = groups;
+		}
 
-				groups.push(group)
-			}
-		});
-
-		jsonPayload.groups = groups;
 		return jsonPayload;
 	},
 
@@ -46,7 +51,6 @@ export default StorageAdapter.extend({
 		headers['Accept'] = 'text/plain';
 		return record.get('users').then(function(users){
 			headers[header] = users.map(function(user){
-				console.log('user.id', user.id);
 				return user.id;
 			}).join(',');
 			return self.ajax(self.buildURL(type.typeKey)+'?update=', 'POST');
@@ -60,12 +64,10 @@ export default StorageAdapter.extend({
 
 	deleteRecord: function(store, type, record) {
 		var headers = this.get('headers');
-		var header = 'X-Account-Group-'+record.get('name');
+		var header = 'X-Account-Group-' + encodeURI(record.get('name'));
 		headers['Accept'] = 'text/plain';
 		headers[header] = '~';
 		return this.ajax(this.buildURL(type.typeKey)+'?update=', 'POST');
-
 	}
-
 });
 
