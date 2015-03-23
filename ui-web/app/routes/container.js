09a8c4f6f8fcaec87051555c80b1fb787ea7e659
Olga Brani <olgabrani@grnet.gr>
2015-03-23 15:09:04 +0200

    Move top-bar to application template

--- ui-web/app/routes/container.js
+++ ui-web/app/routes/container.js
@@ -4,11 +4,15 @@ import ResetScrollMixin from '../mixins/reset-scroll';
 export default Ember.Route.extend(ResetScrollMixin,{
   renderTemplate: function(){
     this.render('container');
-    this.render('bar', {
-      into: 'container',
-      outlet: 'bar',
+    this.render('bar-rt-objects', {
+      into: 'application',
+      outlet: 'bar-rt',
+      controller: 'objects',
+    });
+    this.render('bar-lt-objects', {
+      into: 'application',
+      outlet: 'bar-lt',
       controller: 'objects',
     });
   }
-
 });
