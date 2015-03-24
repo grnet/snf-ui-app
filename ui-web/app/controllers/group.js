61c59cb82366228dd3534b38611ab44e0035402f
Athina Bekakou <athinabek@grnet.gr>
2015-03-24 14:24:17 +0200

    Refactor the mechanism of adding members into existing group
    Add the appropriate input view and adjust the GroupController.
    
    This is related with olgabrani/synnefo#114.

--- ui-web/app/controllers/group.js
+++ ui-web/app/controllers/group.js
@@ -1,8 +1,93 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
+  usersExtended: undefined,
+  allUsersValid: false,
+  cleanUserInput: true,
+
+  init: function() {
+    this.set('usersExtended', []);
+    this._super();
+  },
+
+  areUsersValid: function() {
+    var allUsersValid = this.get('usersExtended').every(function(user, index) {
+      return user.get('status') === 'success';
+    });
+    if(this.get('usersExtended').get('length')) {
+      this.set('allUsersValid', allUsersValid);
+    }
+    else {
+      this.set('allUsersValid', false);
+    }
+
+  }.observes('usersExtended.@each', 'usersExtended.@each.status'),
+
+    freezeCreation: function() {
+
+    var allUsersValid = this.get('allUsersValid');
+    var cleanUserInput = this.get('cleanUserInput');
+
+    return !(allUsersValid && cleanUserInput);
+  }.property('allUsersValid', 'cleanUserInput'),
 
   actions: {
+    addUser: function(user) {
+      var usersExtended = this.get('usersExtended');
+      var notInserted = usersExtended.filterBy('email', user.email).get('length') === 0;
+      var notMember = this.get('users').filterBy('email', user.email).get('length') === 0;
+      if(notInserted && notMember) {
+
+        var userExtended = Ember.Object.create({
+          email: user.email,
+          status: user.status,
+          errorMsg: user.errorMsg,
+        });
+        this.get('usersExtended').pushObject(userExtended)
+
+        if(user.status !== 'error') {
+          this.send('findUser', user.email);
+        }
+      }
+    },
+
+    updateUser: function(email, data) {
+
+      for(var prop in data) {
+        this.get('usersExtended').findBy('email', email).set(prop, data[prop]);
+      }
+
+    },
+
+    removeUser: function(email) {
+
+      var user = this.get('usersExtended').findBy('email', email);
+
+      this.get('usersExtended').removeObject(user);
+
+    },
+
+    findUser: function(email) {
+
+      var self = this;
+      var userEmail = 'email='+email;
+
+      this.store.find('user', userEmail).then(function(user) {
+
+        var userExtended = self.get('usersExtended').findBy('email', email);
+
+          if(userExtended) {
+            self.send('updateUser', email, {uuid: user.get('uuid'), status: 'success'});
+          }
+    },function(error) {
+
+        var userExtended = self.get('usersExtended').findBy('email', email);
+
+          if(userExtended) {
+            self.send('updateUser', email, {uuid: undefined, status: 'error', 'errorMsg': 'Not found'});
+          }
+      });
+    },
     deleteGroup: function(){
       var group = this.get('model');
 
@@ -16,7 +101,6 @@ export default Ember.ObjectController.extend({
       };
 
       group.deleteRecord();
-
       group.save().then(onSuccess, onFail);
     },
 
@@ -43,36 +127,33 @@ export default Ember.ObjectController.extend({
       }, onFail);
    },
 
-    addUsers: function(){
-      var self = this;
-      var group = this.get('model');
-      var newEmails = this.get('newEmails') || '';
-      if (!newEmails.trim()) { return; }
+    addUsersToGroup: function(){
+      if(!this.get('freezeCreation')) {
+        var self = this;
+        var group = this.get('model');
+        var uuids = this.get('usersExtended').mapBy('uuid');
 
-      newEmails = newEmails.split(',');
-      if (newEmails.length <1 ) { return; }
+        self.store.filter('user', function(user) {
+          var id = user.get('id');
+          if(uuids.indexOf(id) !== -1) {
+            return user;
+          }
+        }).then(function(newMembers) {
 
-      var onSuccess = function(data) {
-        self.set('newEmails', '');
-      };
- 
-      var onFail = function(reason){
-        console.log('reason:', reason);
-        self.send('showActionFail', reason);
-      };
+            group.get('users').then(function(users){
+              newMembers.forEach(function(user) {
+                users.pushObject(user)
+              });
 
-      var newUsers = newEmails.map(function(email) {
-        var userEmail = 'email='+email.trim();
-        return self.store.find('user', userEmail);
-      });
-
-      return Ember.RSVP.all(newUsers).then(function(res){ 
-        group.get('users').pushObjects(res).then(function() {
-          group.save().then(onSuccess, onFail);
+              group.save().then(function(){
+                self.send('resetCreation');
+              }, function(error) {
+                self.send('showActionFail', error)
+                console.log('ERROR!')
+              });
+            });
         });
-      }, onFail);
-
+      }
     }
-
   },
 });
