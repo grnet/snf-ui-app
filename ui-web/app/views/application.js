0dee3881650023e1d300e6f19cdd89fcbd5bbd17
Olga Brani <olgabrani@grnet.gr>
2015-04-02 16:17:57 +0300

    Position `fixed` for top-bar on scroll

---
+++ ui-web/app/views/application.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.View.extend({
+  didInsertElement: function(){
+
+    var stickyOffset = $('.sticky').offset().top;
+
+    $(window).scroll(function(){
+      var sticky = $('.sticky'),
+          scroll = $(window).scrollTop();
+            
+      if (scroll >= stickyOffset) {
+        sticky.addClass('fixed');
+      } else {
+        sticky.removeClass('fixed');
+      }
+
+    });
+
+  },
+});
