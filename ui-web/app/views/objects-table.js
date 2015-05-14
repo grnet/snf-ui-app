d5d348915adb78115ef29a12b09d1899db92aadc
Olga Brani <olgabrani@grnet.gr>
2015-05-14 13:15:20 +0300

    Remove unused files

--- ui-web/app/views/objects-table.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-import TableMixin from '../mixins/table';
-
-export default Ember.View.extend( TableMixin, {
-  templateName: 'objects-table',
-
-});
--- ui-web/app/views/table-list.js
+++
@@ -1,6 +0,0 @@
-import Ember from 'ember';
-import TableMixin from '../mixins/table';
-
-export default Ember.View.extend( TableMixin, {
-  templateName: 'table-list',
-});
