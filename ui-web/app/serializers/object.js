63bceb31acef768928dc4baec6aeb06d995e3782
Olga Brani <olgabrani@grnet.gr>
2015-06-02 16:44:24 +0300

    Use keyForRelationship in object serializer

--- ui-web/app/serializers/object.js
+++ ui-web/app/serializers/object.js
@@ -9,26 +9,23 @@ export default DS.RESTSerializer.extend({
     return mapping[attr] || attr;
   },
 
+  keyForRelationship: function(attr, belongsTo) {
+    var mapping = {
+      'modified_by': 'x_object_modified_by'
+    };
+    return mapping[attr] || attr;
+  },
 
   extractArray: function(store, type, payload) {
     var container_id = payload.container_id;
     delete payload.container_id;
     var payload_list = payload;
     payload_list.forEach(function(el){
-      el.modified_by = el.x_object_modified_by;
-      delete el.x_object_modified_by;
       el.id = container_id+'/'+el.name;
     });
     payload = { objects: payload_list};
     return this._super(store, type, payload);
   },
 
- 
-  extractSingle: function(store, type, payload, id) {
-    payload.modified_by = payload.x_object_modified_by;
-    delete payload.x_object_modified_by;
-
-    return this._super(store, type, payload, id);
-  }
 
 });
