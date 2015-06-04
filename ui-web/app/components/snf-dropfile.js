9452224d03ff9420854b8b9ba8e2291a3cf6781f
Athina Bekakou <athinabek@grnet.gr>
2015-06-04 13:18:40 +0300

    Replace tooltips in dropfile area with foundation tooltips

--- ui-web/app/components/snf-dropfile.js
+++ ui-web/app/components/snf-dropfile.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
 import DropComponent from '../snf/dropfile/component';
+import {TooltipViewMixin} from '../mixins/tooltip';
 
-export default DropComponent.extend();
+export default DropComponent.extend(TooltipViewMixin);
