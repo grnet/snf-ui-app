6ad20ef7a890d0dc5b0849631485da8c98f04fd0
Olga Brani <olgabrani@grnet.gr>
2015-06-04 22:37:08 +0300

    Improve sidebar dir-tree
    * Use different name template for shared with me dir-tree.
    * Set icon in the controller instead of having conditionals in the template.
    * Do not always let name be a link.

--- ui-web/app/components/dir-tree.js
+++ ui-web/app/components/dir-tree.js
@@ -6,19 +6,30 @@ export default Ember.Component.extend({
   
   name: function(){
     var root = this.get('root');
-    if (root == '/'){
-      return 'My folders';
-    } else {
-      return root.split('/').pop();
-    }
+    var res = root.split('/').pop();
+    if (root == '/') { res = 'My folders';}
+    if (root == 'shared') { res = 'Shared with me';}
+    return res;
   }.property('root'),
 
-  is_root: function(){
-    return this.get('root') == '/';
-  }.property('root'),
+  notLink: function(){
+    /*  Do not show link if 
+        - It is a user sharing objects
+        - It is the root of the shared with me objects
+        - It is the parent of containers ('/')
+    */
+    var b = this.get('root') === '/';
+    var c = this.get('root') === 'shared';
+    return this.get('is_user') || b || c;
+  }.property('root', 'is_user'),
 
   is_container: function(){
     return this.get('root').indexOf('/') == -1;
+  }.property('root'),
+
+  is_user: function(){
+    return this.get('root').indexOf('\/accounts\/') == 0 && 
+            this.get('root').replace('\/accounts\/', '').indexOf('/') === -1;
   }.property('root'),
 
   is_folder: function(){
@@ -29,12 +40,21 @@ export default Ember.Component.extend({
     return this.get('root') === 'trash' && this.get('is_container');  
   }.property('root'),
 
+  iconCls: function(){
+    var res = "fa-folder";
+    if (this.get('expanded')) { res = "fa-folder-open";}
+    if (this.get('isTrash')) { res = "fa-trash";}
+    if (this.get('is_user')) { res = "fa-user";}
+    if (this.get('root') === 'shared') { res = "fa-share-alt"}
+    return res;
+  }.property('isTrash', 'expanded', 'is_user', 'root'),
+
   textTemplateName: function(){
     var role = this.get('role');
     if (role) {
-      return "dir-tree-name-"+role;
+      return "components/dir-tree/name-"+role;
     }
-    return "dir-tree-name";
+    return "components/dir-tree/name";
   }.property('role'),
 
   container_id: function(){
