9dc2f0d1660fe6e7cac1e64145901d0d717cba99
Athina Bekakou <athinabek@grnet.gr>
2015-06-04 15:32:51 +0300

    Replace tooltips of containers with foundation tooltips

--- ui-web/app/views/container.js
+++ ui-web/app/views/container.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import {TooltipViewMixin} from '../mixins/tooltip';
 
-export default Ember.View.extend({
+export default Ember.View.extend(TooltipViewMixin, {
 	layoutName: 'container',
 	tagName: 'li',
 	classNameBindings: ['isNew', 'isLoading', 'isTrash'],
