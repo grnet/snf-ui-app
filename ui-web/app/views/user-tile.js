5a967490d52302affd68a65aeb2b9be47a4b88a6
Athina Bekakou <athinabek@grnet.gr>
2015-06-04 13:08:01 +0300

    Replace tooltips of user-tiles with foundation tooltips

--- ui-web/app/views/user-tile.js
+++ ui-web/app/views/user-tile.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import {TooltipViewMixin} from '../mixins/tooltip';
 
-export default Ember.View.extend({
+export default Ember.View.extend(TooltipViewMixin, {
 	templateName: 'user-tile',
 	tagName: 'span',
 	classNames: ['tile'],
