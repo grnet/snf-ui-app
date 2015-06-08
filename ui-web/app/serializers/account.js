f2d5d4b3cb25ff5797c612aaaa52d14358b7dc96
Olga Brani <olgabrani@grnet.gr>
2015-06-08 14:52:24 +0300

    Handle adapter's empty shared with me payload

--- ui-web/app/serializers/account.js
+++ ui-web/app/serializers/account.js
@@ -2,13 +2,16 @@ import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend({
   extract: function(store, type, payload) {
-    var entries = _.select(payload.split("\n"));
-    return entries.map(function(account) {
-      return {
-        id: account,
-        uuid: account,
-        user: account
-      };
-    });
+    if (payload) {
+      var entries = _.select(payload.split("\n"));
+      return entries.map(function(account) {
+        return {
+          id: account,
+          uuid: account,
+          user: account
+        };
+      });
+    }
+    return [];
   }
 });
