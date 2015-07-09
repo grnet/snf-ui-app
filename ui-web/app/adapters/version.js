da49818087bb64c67b1d9bd2433188c6fd622c0d
Olga Brani <olgabrani@grnet.gr>
2015-07-09 17:18:38 +0300

    Refactor versions adapter

--- ui-web/app/adapters/version.js
+++ ui-web/app/adapters/version.js
@@ -3,9 +3,12 @@ import StorageAdapter from 'ui-web/snf/adapters/storage';
 export default StorageAdapter.extend({
 
   findQuery: function(store, type, query) {
+    var parts, id;
     var object_id = store.get('object_id');
     delete store.object_id;
-
-    return this.ajax(this.buildURL(type.typeKey, object_id), 'GET', { data: query });
+    parts = object_id.split('/');
+    parts.shift();
+    id = parts.join('/');
+    return this.ajax(this.buildURL(type.typeKey, id), 'GET', { data: query });
   },
 });
