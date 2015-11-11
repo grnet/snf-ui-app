c790e57cd1cd5102b8b6ea5332c6030aba068155
Athina Bekakou <athinabek@grnet.gr>
2015-11-11 16:41:43 +0200

    Fix indentation

--- snf-ui/app/views/dialog.js
+++ snf-ui/app/views/dialog.js
@@ -1,13 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.View.extend({
-	classNames: ['reveal-modal'],
-	classNameBindings: ['templateCls'],
-	attributeBindings: ['data-reveal'],
-	'data-reveal': 'true',
-	layoutName: 'overlays/dialog-wrapper',
-	needReset: ['overlays.groups', 'overlays.sharing'],
-	rerenderPopovers: false,
+  classNames: ['reveal-modal'],
+  classNameBindings: ['templateCls'],
+  attributeBindings: ['data-reveal'],
+  'data-reveal': 'true',
+  layoutName: 'overlays/dialog-wrapper',
+  needReset: ['overlays.groups', 'overlays.sharing'],
+  rerenderPopovers: false,
 
 
   /*  Assign a class to each dialog
@@ -34,42 +34,41 @@ export default Ember.View.extend({
     return clsMap[this.get('renderedName')];
   }.property('renderedName'),
 
-	revealDialog: function() {
-		this.$().foundation('reveal', 'open', {
-			animation: 'fade',
-			animation_speed: 250,
-		});
-	}.on('didInsertElement'),
+  revealDialog: function() {
+    this.$().foundation('reveal', 'open', {
+      animation: 'fade',
+      animation_speed: 250,
+    });
+  }.on('didInsertElement'),
 
-	/*
-	 * CloseDialog can be used if you want to trigger the closing action
-	 * from the controller.
-	 * For example, use it if you want to do reset actions in the views,
-	 * that are included inside the dialog, before the dialog gets hidden
-	 * or removed from the DOM. In this case the controller that is related
-	 * with the dialog must have the property closeDialog. This prop will
-	 * be updated when all child-views finish with their actions.
-	*/
-	closeDialog: function() {
-		var closeDialog = this.get('controller').get('closeDialog');
+  /*
+   * CloseDialog can be used if you want to trigger the closing action
+   * from the controller.
+   * For example, use it if you want to do reset actions in the views,
+   * that are included inside the dialog, before the dialog gets hidden
+   * or removed from the DOM. In this case the controller that is related
+   * with the dialog must have the property closeDialog. This prop will
+   * be updated when all child-views finish with their actions.
+  */
+  closeDialog: function() {
+    var closeDialog = this.get('controller').get('closeDialog');
 
-		if(closeDialog && this.get('_state') === 'inDOM') {
-			this.$().foundation('reveal', 'close');
-			this.get('controller').set('closeDialog', false)
-		}
-	}.observes('controller.closeDialog'),
+    if(closeDialog && this.get('_state') === 'inDOM') {
+      this.$().foundation('reveal', 'close');
+      this.get('controller').set('closeDialog', false)
+    }
+  }.observes('controller.closeDialog'),
 
-	didInsertElement: function() {
+  didInsertElement: function() {
 
-		this._super();
+    this._super();
 
-		var self = this;
+    var self = this;
+    // templateName could be: overlays.error or overlays.feedback etc
+    var templateName = this.get('renderedName');
 
-		// templateName could be: overlays.error or overlays.feedback etc
-		var templateName = this.get('renderedName');
-
-		// type is used to disconnect the dialog from the correct outlet
-		var type = templateName.replace('overlays.', '');
+    // type is used to disconnect the dialog from the correct outlet
+    var type = templateName.replace('overlays.', '');
 
     self.$().foundation('reveal', {
       animation: 'fade',
@@ -97,78 +96,75 @@ export default Ember.View.extend({
         else if(self.get('controller').get('name_stripped') === 'object/copy' || self.get('controller').get('name_stripped') === 'object/restore' || self.get('controller').get('name_stripped') === 'object/cut') {
             self.get('controller').send('unSelectDir');
         }
+      }
+    });
 
-			}
-		});
+    $(document).on('closed.fndtn.reveal', '[data-reveal]', function () {
+        // this bubbles up to application route
+        self.get('controller').send('removeDialog', type);
+    });
 
-		$(document).on('closed.fndtn.reveal', '[data-reveal]', function () {
-				// this bubbles up to application route
-				self.get('controller').send('removeDialog', type);
-		});
-
-	    $(document).on('opened.fndtn.reveal', '[data-reveal]', function () {
-	      var dialog = $(this);
-	      dialog.find('[autofocus]').focus();
-	    });
-	},
-	/*
-	 * Every event handler that has bound with the current view should be removed
-	 * before the view gets destroyed
-	 */
-	willDestroyElement: function() {
-	    $(document).find('.reveal-modal-bg').remove();
-	    $(document).off('click', '[data-reveal] .js-btn-slide');
-	    // check off(close)
-	    $(document).off('close.fndtn.reveal', '[data-reveal]');
-	    // check off(closed)
-	    $(document).off('closed.fndtn.reveal', '[data-reveal]');
-	    $(document).off('opened.fndtn.reveal', '[data-reveal]');
-	},
+      $(document).on('opened.fndtn.reveal', '[data-reveal]', function () {
+        var dialog = $(this);
+        dialog.find('[autofocus]').focus();
+      });
+  },
+  /*
+   * Every event handler that has bound with the current view should be removed
+   * before the view gets destroyed
+   */
+  willDestroyElement: function() {
+    $(document).find('.reveal-modal-bg').remove();
+    $(document).off('click', '[data-reveal] .js-btn-slide');
+    // check off(close)
+    $(document).off('close.fndtn.reveal', '[data-reveal]');
+    // check off(closed)
+    $(document).off('closed.fndtn.reveal', '[data-reveal]');
+    $(document).off('opened.fndtn.reveal', '[data-reveal]');
+  },
 
   confirm_title: function(){
     return this.get('controller.verb_for_action') + ' ' + this.get('controller.itemType');
   }.property('controller.verb_for_action', 'controller.itemType'),
 
 
-	actions: {
-		/*
-		 * slide action slides down a closed area and focuses the 1st input in it
-		 * or if the area is open, it slides it up and send to the corresponting
-		 * controller the action reset
-		*/
-		slide: function(controller, areaID, action) {
-			var self = this;
-			var $area = this.$('.slide-container#'+areaID);
-			$area.toggleClass('open');
-			var toOpen = $area.hasClass('open');
+  actions: {
+    /*
+     * slide action slides down a closed area and focuses the 1st input in it
+     * or if the area is open, it slides it up and send to the corresponting
+     * controller the action reset
+    */
+    slide: function(controller, areaID, action) {
+      var self = this;
+      var $area = this.$('.slide-container#'+areaID);
+      $area.toggleClass('open');
+      var toOpen = $area.hasClass('open');
 
-			if(action) {
-				controller.send(action);
-			}
+      if(action) {
+        controller.send(action);
+      }
 
-			if(toOpen) {
-				$area.find('.js-slide-me').stop().slideDown('slow', function() {
-					$area.find('input:first').focus();
-					self.set('rerenderPopovers', true);
-					console.log('-----')
-				});
-			}
-			else {
-				$area.find('.js-slide-me').stop().slideUp('slow', function() {
-					if(controller.get('name_stripped') === 'groups') {
-						controller.send('reset');
-					}
-				});
-			}
-
-
-		}
-	},
-
-	deleteGroup: {
-		title: 'Delete Group',
-		action_verb: 'delete'
-	}
+      if(toOpen) {
+        $area.find('.js-slide-me').stop().slideDown('slow', function() {
+          $area.find('input:first').focus();
+          self.set('rerenderPopovers', true);
+          console.log('-----')
+        });
+      }
+      else {
+        $area.find('.js-slide-me').stop().slideUp('slow', function() {
+          if(controller.get('name_stripped') === 'groups') {
+            controller.send('reset');
+          }
+        });
+      }
 
 
+    }
+  },
+
+  deleteGroup: {
+    title: 'Delete Group',
+    action_verb: 'delete'
+  }
 });
