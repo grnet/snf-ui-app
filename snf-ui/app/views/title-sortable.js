363455b48957fa1a243c10a08f79657a345a008a
Olga Brani <olgabrani@grnet.gr>
2015-11-12 19:34:51 +0200

    Proper align objects list header
    Alter `title-sortable` view so as to include toggle selection checkbox
    inside the view for styling purposes.

--- snf-ui/app/views/title-sortable.js
+++ snf-ui/app/views/title-sortable.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.View.extend({
-  templateName: 'title-sortable',
+  layoutName: 'title-sortable',
   classNames: ['col', 'sortable'],
 
   title: function(){
