cf867868a8267ed64279dcf77ae085149e1603be
Kostas Papadimitriou <kpap@grnet.gr>
2015-11-25 14:26:38 +0200

    Revert slash escaping for all storage based adapters
    fixes object version fetch url

--- snf-ui/app/snf/adapters/storage.js
+++ snf-ui/app/snf/adapters/storage.js
@@ -8,6 +8,28 @@ import SnfRestAdapter from 'snf-ui/snf/adapters/base';
  */
 export default SnfRestAdapter.extend({
   account: Ember.computed.alias('settings.uuid'),
+  
+  buildURL: function(type, id, snapshot) {
+    var url = [];
+    var host = Ember.get(this, 'host');
+    var prefix = this.urlPrefix();
+
+    if (type) { url.push(this.pathForType(type)); }
+
+    //We might get passed in an array of ids from findMany
+    //in which case we don't want to modify the url, as the
+    //ids will be passed in through a query param
+    if (id && !Ember.isArray(id)) { 
+      url.push(encodeURIComponent(id).replace(/%2f/gi, "/")); 
+    }
+
+    if (prefix) { url.unshift(prefix); }
+
+    url = url.join('/');
+    if (!host && url) { url = '/' + url; }
+
+    return url;
+  },
 
   apiKey: 'storage',
   typePathMap: function() {
