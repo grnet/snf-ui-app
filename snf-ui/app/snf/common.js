1b6fd9d224e4ad4473b0aba0d6537985f8255e97
Athina Bekakou <athinabek@grnet.gr>
2016-05-23 14:37:07 +0300

    Handle negative bytes in bytesToHuman function

--- snf-ui/app/snf/common.js
+++ snf-ui/app/snf/common.js
@@ -15,7 +15,7 @@ export function bytesToHuman(bytes){
   if (bytes === 0) { 
     return '0';
   }
-  var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+  var i = parseInt(Math.floor(Math.log(Math.abs(bytes)) / Math.log(1024)));
   return (bytes / Math.pow(1024, i)).toFixed(1) + ' ' + sizes[i];
 }
 
