d980311b12e3d14c815ed53f688ad779f1050d51
Athina Bekakou <athinabek@grnet.gr>
2016-05-25 16:35:11 +0300

    Modify usage related views
    - Improve style
    - Refactor code
    - Modify the location of data in usage overlay

--- snf-ui/app/views/quotas-per-project.js
+++ snf-ui/app/views/quotas-per-project.js
@@ -4,33 +4,9 @@ export default Ember.View.extend({
 	templateName: 'overlays/quotas-per-project',
 	tagName: 'li',
 	minWidth: 2, //percent
-	// If the diskspace usage is more then 80% but not more then 100% returns true
-	isNearFull: function() {
-		if((this.get('usage_percentage') > 80) && (this.get('usage_percentage') <= 100)) {
-			return true;
-		}
-		return false;
-	}.property('usage_percentage'),
-
-	// If the the usage is more then the available quotas returns true
-	isOverquota: function() {
-		if(this.get('project').get('disk_overquota_space') > 0) {
-			return true;
-		}
-		return false;
-	}.property('disk_overquota_space'),
-
-	usage_percentage: function() {
-		var usage = this.get('project').get('diskspace_user_usage');
-		var limit = this.get('project').get('diskspace_effective_limit');
-		if(limit > usage) {
-			return ((usage/limit)*100).toFixed(2);
-		}
-		return 100;
-	}.property('diskspace_user_usage', 'diskspace_effective_limit'),
 
 	width_usage: function(){
-		var usage_percentage = this.get('usage_percentage');
+		var usage_percentage = this.get('project').get('usage_percentage');
 		var min_width = this.get('minWidth'); 
 		if(usage_percentage < min_width && usage_percentage) {
 			return 'width:' + min_width + '%';
@@ -41,10 +17,10 @@ export default Ember.View.extend({
 		else {
 			return 'width:' + usage_percentage + '%';
 		}
-	}.property('usage_percentage'),
+	}.property('project.usage_percentage'),
 
 	width_free: function() {
-		var usage_percentage = this.get('usage_percentage');
+		var usage_percentage = this.get('project').get('usage_percentage');
 		var min_width = this.get('minWidth');
 		if(usage_percentage < min_width) {
 			return 'width:' + (100 - min_width) + '%';
@@ -55,8 +31,9 @@ export default Ember.View.extend({
 		else {
 			return 'width:' + (100 - usage_percentage) + '%';
 		}
-	}.property('usage_percentage'),
-	posLabel: function() {
+	}.property('project.usage_percentage'),
+
+	position_label: function() {
 		if(!this.get('parentView').get('no-display')) {
 			var sizeWidth = this.$('.size').outerWidth();
 			var meterWidth = this.$('.meter').width();
