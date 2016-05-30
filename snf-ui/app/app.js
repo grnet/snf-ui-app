26e36ca8723d6cc9f76524ab47e380bf7ba0f569
Kostas Papadimitriou <kpap@grnet.gr>
2016-05-30 14:17:02 +0300

    Object.is polyfill

--- snf-ui/app/app.js
+++ snf-ui/app/app.js
@@ -11,6 +11,20 @@ if (typeof global !== 'undefined' && global.moment) { window.moment = global.mom
 // FF String.includes
 String.prototype.includes = String.prototype.includes || String.prototype.contains;
 
+// Object.is polyfill
+if (!Object.is) {
+  Object.is = function(x, y) {
+    // SameValue algorithm
+    if (x === y) { // Steps 1-5, 7-10
+      // Steps 6.b-6.e: +0 != -0
+      return x !== 0 || 1 / x === 1 / y;
+    } else {
+      // Step 6.a: NaN == NaN
+      return x !== x && y !== y;
+    }
+  };
+}
+
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 var App = Ember.Application.extend({
