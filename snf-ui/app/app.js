963da6b4f022eddd962b97d40ca7da5411afe02e
Athina Bekakou <athinabek@grnet.gr>
2016-06-10 17:06:13 +0300

    Add support for browsers
    Polyfills for String.includes, Array.includes, String.repeat.

--- snf-ui/app/app.js
+++ snf-ui/app/app.js
@@ -8,8 +8,83 @@ import SynnefoStore from './store/synnefo-store';
 // hacky way to suitably inject momentjs in window scope
 if (typeof global !== 'undefined' && global.moment) { window.moment = global.moment; }
 
-// FF String.includes
-String.prototype.includes = String.prototype.includes || String.prototype.contains;
+// FF 18 - 39, IE, Opera, Safari < 9 String.includes
+String.prototype.includes = String.prototype.includes || String.prototype.contains || function() { return String.prototype.indexOf.apply( this, arguments ) !== -1; };
+
+// Array.includes polyfill
+if (!Array.prototype.includes) {
+  Array.prototype.includes = function(searchElement /*, fromIndex*/ ) {
+    'use strict';
+    var O = Object(this);
+    var len = parseInt(O.length, 10) || 0;
+    if (len === 0) {
+      return false;
+    }
+    var n = parseInt(arguments[1], 10) || 0;
+    var k;
+    if (n >= 0) {
+      k = n;
+    } else {
+      k = len + n;
+      if (k < 0) {k = 0;}
+    }
+    var currentElement;
+    while (k < len) {
+      currentElement = O[k];
+      if (searchElement === currentElement) { // NaN !== NaN
+        return true;
+      }
+      k++;
+    }
+    return false;
+  };
+}
+
+// String.repeat polyfill
+if (!String.prototype.repeat) {
+  String.prototype.repeat = function(count) {
+    'use strict';
+    if (this == null) {
+      throw new TypeError('can\'t convert ' + this + ' to object');
+    }
+    var str = '' + this;
+    count = +count;
+    if (count != count) {
+      count = 0;
+    }
+    if (count < 0) {
+      throw new RangeError('repeat count must be non-negative');
+    }
+    if (count == Infinity) {
+      throw new RangeError('repeat count must be less than infinity');
+    }
+    count = Math.floor(count);
+    if (str.length == 0 || count == 0) {
+      return '';
+    }
+    // Ensuring count is a 31-bit integer allows us to heavily optimize the
+    // main part. But anyway, most current (August 2014) browsers can't handle
+    // strings 1 << 28 chars or longer, so:
+    if (str.length * count >= 1 << 28) {
+      throw new RangeError('repeat count must not overflow maximum string size');
+    }
+    var rpt = '';
+    for (;;) {
+      if ((count & 1) == 1) {
+        rpt += str;
+      }
+      count >>>= 1;
+      if (count == 0) {
+        break;
+      }
+      str += str;
+    }
+    // Could we try:
+    // return Array(count + 1).join(this);
+    return rpt;
+  }
+}
+
 
 // Object.is polyfill
 if (!Object.is) {
