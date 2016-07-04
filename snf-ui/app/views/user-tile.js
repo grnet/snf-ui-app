e325f65e20d4ed3c2c44ec098bb2009c5dca02b8
Athina Bekakou <athinabek@grnet.gr>
2016-07-04 15:36:16 +0300

    Modify style of user tile
    - Replace "x" sign with trash icon
    - Use variables of colors for font, background and borders

--- snf-ui/app/views/user-tile.js
+++ snf-ui/app/views/user-tile.js
@@ -4,8 +4,18 @@ export default Ember.View.extend({
 	templateName: 'user-tile',
 	tagName: 'span',
 	classNames: ['tile'],
-	classNameBindings: ['status'],
-	status: function() {
-		return this.get('user.status');
+	classNameBindings: ['status_cls'],
+	status_cls: function() {
+		// status values: success, loading, error
+		var status = this.get('user.status');
+		if(status === 'success') {
+			return 'success-revert';
+		}
+		else if (status === 'error') {
+			return 'danger-revert';
+		}
+		else {
+			return status;
+		}
 	}.property('user.status')
 });
